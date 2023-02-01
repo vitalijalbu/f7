35389c9d5b83967091fa560efbe9d13e43220225
Vitalie Jalbu <40239902+vitalijalbu@users.noreply.github.com>
2023-02-01 15:43:33 +0100

    build

---
+++ www/_next/static/chunks/pages/account-25d02182a52d53fc.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7966,1348],{6160:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/account",function(){return u(8750)}])}},function(n){n.O(0,[6175,8750,9774,2888,179],function(){return n(n.s=6160)}),_N_E=n.O()}]);
---
+++ www/_next/static/chunks/pages/listings/[id]-1acc315bcd5afb1d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7748,7873],{102:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/listings/[id]",function(){return u(7559)}])}},function(n){n.O(0,[6175,3649,8418,7559,9774,2888,179],function(){return n(n.s=102)}),_N_E=n.O()}]);
